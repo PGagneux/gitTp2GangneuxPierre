e40c0a1dfec0f0609b6c6d681371b89aa68fc1b9
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-02-15 09:36:20 +0100

    ajout du fichier TestsPetitsCalculs.java

---
+++ TestsPetitsCalculs.java
@@ -0,0 +1,43 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+public class TestsPetitsCalculs{
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testMultiplication1() {
+        assertEquals(72, 8 * 9);
+    }
+    @Test
+    public void testMultiplication2() {
+        assertEquals(1664, 32 * 52);
+    }
+    @Test
+    public void testHexa() {
+        assertEquals(256, 0x10 * 0x10);
+    }
+
+    @Test
+    public void testLength(){
+        assertEquals("abc".length(), 3);
+    }
+    
+    public String inverseMot(String leMot){
+        String inverse = "";
+        for (int i=leMot.length()-1; i>=0; i -= 1){
+            inverse = inverse + leMot.charAt(i); 
+        }
+        return inverse;
+    }
+
+    @Test
+    public void testInverseMot(){
+        assertNotEquals(inverseMot("abc"), "ab");
+        assertEquals(inverseMot("abc"), "cba");
+    }
+
+
+}
